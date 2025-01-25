55c433cea7db10cb89aab5f0bf6f59bce9dd12b2
aa43000 <ahmadzaenimubarok121@gmail.com>
2025-01-25 10:40:20 +0700

    1-pengenalan-php

---
+++ 1-pengenalan-php.php
@@ -0,0 +1,5 @@
+PHP -> Personal Home Page -> PHP: Hypertext Processor
+1. bahasa pemrograman di sisi server/backend
+2. mengatur logika-logika
+3. berinteraksi ke database
+4. membuat web menjadi dinamis
